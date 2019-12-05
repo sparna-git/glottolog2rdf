811736ef3e7142dd16209e3a41bb6fdfacc0073a
lluc <leger.luc@gmail.com>
2019-12-05 08:20:00 +0100

    Fist test

--- tests/test_glottolog2rdf_converter.py
+++ tests/test_glottolog2rdf_converter.py
@@ -5,4 +5,7 @@ from glottolog2rdf_converter import Glottolog2RdfConverter
 
 
 def test_parse_glottolog():
+    cvt = Glottolog2RdfConverter(
+        glottolog_dir='samples/glottolog-single-languoid/')
+    cvt.parse_glottolog()
     assert True
